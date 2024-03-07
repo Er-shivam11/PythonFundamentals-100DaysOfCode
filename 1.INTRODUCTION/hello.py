9a8e812e849fc1865d0f3cebaa212f747d74e18c
shivam <shivamnirmal.097@gmail.com>
2024-03-07 19:17:19 +0530

    basic setup

---
+++ 1.INTRODUCTION/hello.py
@@ -0,0 +1,7 @@
+print("welcome to shivam bytes folks")
+# ctrl+s to save and open terminal 
+# now you can see successfully we run the first hello program, now lets run in python shell
+# so you can see i share example where oyu can print simple word in single or double inverted commas but make sure both side should be same means 
+'' or " "
+# now lets run same script on cmd terminal 
+# Thank you.
