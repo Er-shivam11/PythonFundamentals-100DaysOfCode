6e497a9493206dd615952ccf9e3816c26e996cc9
shivam <shivamnirmal.097@gmail.com>
2024-03-09 12:55:13 +0530

    done with casting

---
+++ 3.VAR&DT/casting.py
@@ -0,0 +1,19 @@
+# casting 
+
+# INTEGERS
+var_a='t'
+
+# FLOAT
+var_b=3.9
+
+# float to integer casting
+c=int(var_b)
+print(c)
+
+# int to float
+d=float(var_a)
+print(d)
+# ctrl+save
+# see error that means if int is number then 
+# it will convert into float but as string if we will pass it will not cast.
+
---
+++ 3.VAR&DT/numbers.py
@@ -0,0 +1,36 @@
+# INTEGERS
+var_a=2
+
+# FLOAT
+var_b=3.5
+
+# addition
+c=var_a+var_b
+
+# substraction
+d=var_b-var_a
+
+# multiplication
+e=var_a*var_b
+
+# division
+f=var_b/var_a
+
+# exponential
+g=var_b**var_a
+
+# modulas
+h=var_b%var_a
+
+# now lets print these numbers to see how they works out!!
+
+print('addition of numbers are:',c)
+print('substraction of numbers are:',d)
+print('multiplication of numbers are:',e)
+print('division of numbers are:',f)
+print('exponent of numbers are:',g)
+print('modulas of numbers are:',h)
+
+# save and run the program now
+# see you in next video!!!
+
